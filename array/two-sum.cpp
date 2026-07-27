56e5c214646bd7f2626d5d57a781518616554f99
Cola <hdmortalnil@gmail.com>
2026-07-27 23:53:39 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.cpp
+++ array/two-sum.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // answer
         int len = nums.size();
-        for (i = 0; i < len; i++) {
-            for (j = i + 1; j < len; j++) {
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
                 if (nums[i] + nums[j] == targe) {
                     return [i,j];
                 }
