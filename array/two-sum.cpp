8ab854e68279d948182ff5a84835fec3624fc976
Cola <hdmortalnil@gmail.com>
2026-07-27 23:53:57 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.cpp
+++ array/two-sum.cpp
@@ -5,7 +5,7 @@ public:
         int len = nums.size();
         for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
-                if (nums[i] + nums[j] == targe) {
+                if (nums[i] + nums[j] == target) {
                     return [i,j];
                 }
             }
