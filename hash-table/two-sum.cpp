c189b17b7f29d13461c213c76f138d99d3ebec26
Cola <hdmortalnil@gmail.com>
2026-07-27 23:55:10 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -6,7 +6,7 @@ public:
         for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return [i,j];
+                    return {i,j};
                 }
             }
         }
