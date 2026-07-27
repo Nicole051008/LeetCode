4c6775bac4514fce892a5e32dd9f3999eb6797d2
Cola <hdmortalnil@gmail.com>
2026-07-27 23:49:15 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.cpp
+++ hash-table/two-sum.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // answer
-        for i in nums{
-            for j in (nums-1){
-                if i+j == target{
-                    return [i,j]
+        int len = nums.size();
+        for (i = 0, i < len; i++) {
+            for (j = i + 1, j < len; j++) {
+                if (nums[i] + nums[j] == targe) {
+                    return [i,j];
                 }
             }
         }
