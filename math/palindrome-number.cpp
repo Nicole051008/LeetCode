4c57535ec47b951218117e6e8ac3e4b449da0482
Cola <hdmortalnil@gmail.com>
2026-07-28 22:23:34 +0800

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int len = x.length();
+        string s = to_string(num);
+        int len = s.length();
         for (int i = 0; i < len; i++){
             if i == len/2{
                 return true;
