b44a7d7ed716380fd538785d2f73171324a4bcef
Cola <hdmortalnil@gmail.com>
2026-07-28 22:25:39 +0800

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -3,14 +3,11 @@ public:
     bool isPalindrome(int x) {
         string s = to_string(x);
         int len = s.length();
-        for (int i = 0; i < len; i++){
-            if (i == len/2){
-                return true;
-            }
+        for (int i = 0; i < len/2-1; i++){
             if (s[i] != s[-i+1]){
                 return false;
             }
         }
-        
+        return true;
     }
 };
