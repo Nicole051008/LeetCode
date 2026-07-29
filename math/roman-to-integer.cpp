018ede9be2407afb084ae7e788efbde547aa4e03
Cola <hdmortalnil@gmail.com>
2026-07-29 14:20:15 +0800

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

---
+++ math/roman-to-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int romanToInt(string s) {
+        unordered_map<char, int> roman_map = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int total = 0;
+        int n = s.length();
+
+        for (int i = 0; i < len - 1; i++){
+            string cur = romanToInt(s[i]);
+            string next = romanToInt(s[i+1]);
+            if (cur < next) {
+                total -= cur;
+            }
+            else {
+                total += cur;
+            }
+        }
+        return total;
+    }
+};
