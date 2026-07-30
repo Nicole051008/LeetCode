184de74fa6469e4687de12fc5c9146a7cf9221e0
Cola <hdmortalnil@gmail.com>
2026-07-30 16:49:23 +0800

    LC-merge-two-sorted-lists [Runtime: N/A; Memory: N/A]

--- linked-list/merge-two-sorted-lists.cpp
+++ linked-list/merge-two-sorted-lists.cpp
@@ -8,22 +8,44 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // 创建虚拟头节点，方便操作
         ListNode dummy(0);
         ListNode* tail = &dummy;
-
+        
+        // 遍历两个链表，每次取较小的节点
         while (list1 != nullptr && list2 != nullptr) {
-            if (list1 -> val >= list2 -> val){
-                tail -> next = list1;
-                list1 = list1 -> next;
+            if (list1->val <= list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
             }
-            else {
-                tail -> next = list2;
-                list2 = list2 -> next;
-            }
+            tail = tail->next;  // 移动 tail 到新节点
         }
+        
+        // 连接剩余节点
+        if (list1 != nullptr) {
+            tail->next = list1;
+        }
+        if (list2 != nullptr) {
+            tail->next = list2;
+        }
+        
+        // 返回真正的头节点
         return dummy.next;
     }
 };
