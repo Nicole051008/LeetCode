6fc721cf7a4add8cb7608d463fd46aad2dc332b3
Cola <hdmortalnil@gmail.com>
2026-07-31 19:28:34 +1000

    LC-remove-duplicates-from-sorted-array [Runtime: N/A; Memory: N/A]

--- array/remove-duplicates-from-sorted-array.cpp
+++ array/remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int len = nums.size();
         int pointer = 1; 
-        int temp = nums[1];
+        int temp = nums[0];
         for (int i = 1; i < len; i ++){
             if (nums[i] != temp) {
                 nums[pointer] = nums[i];
