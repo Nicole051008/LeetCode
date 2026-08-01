222b4bed23d5f8bb9e032169fca9afcbb09e2c85
Cola <hdmortalnil@gmail.com>
2026-08-01 20:43:17 +1000

    LC-add-binary [Runtime: N/A; Memory: N/A]

---
+++ math/add-binary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int pa = a.size() - 1;
+        int pb = b.size() - 1;
+        int flag = 0;
+        string result = "";
+        
+        while ( pa >= 0 || pb >= 0 || flag >0){
+            int sum = flag;
+
+            if (pa >= 0) {
+                sum += a[pa] - '0';
+                pa--; 
+            }
+
+            if (pb >= 0){
+                sum += b[pb] = '0';
+                pb--;
+            }
+
+            flag = sum/2;
+            cur = sum %2;
+            result.push_back(cur + '0');
+
+        }
+        return result;
+        
+    }
+};
