679a4a63e0a35c15ec044302413fe689ef42111e
Cola <hdmortalnil@gmail.com>
2026-08-06 22:54:25 +1000

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.java
+++ math/palindrome-number.java
@@ -8,7 +8,7 @@ class Solution {
         String s = Integer.toString(x);
 
         for (int i = 0; i < s.length() / 2; i++) {
-            for (int j = s.length() - 1; j >= s.length() / 2; j--) {
+            for (int j = s.length(); j >= s.length() / 2; j--) {
 
                 if (s.charAt(i) != s.charAt(j)) {
                     return false;
