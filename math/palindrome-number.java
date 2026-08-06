d9b511b3f81ae781d3ed107cf7bf0a7d4d11e9f4
Cola <hdmortalnil@gmail.com>
2026-08-06 22:27:42 +1000

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.java
+++ math/palindrome-number.java
@@ -5,7 +5,7 @@ class Solution {
         }
         for (int i = 0; i < x.length / 2; i++){
             for (int j = x.length; i> x.length / 2; i--){
-                if x[i] != x[j] {
+                if (x[i] != x[j]) {
                     return false;
                 }
             }
