4b62c4da5624360f2286f4aa92bc1435d0814495
Cola <hdmortalnil@gmail.com>
2026-08-10 22:45:38 +1000

    LC-binary-tree-inorder-traversal [Runtime: N/A; Memory: N/A]

---
+++ tree/binary-tree-inorder-traversal.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        result.add(left.val);
+        result.add(root.val);
+        resutl.add(right.val);
+    }
+}
