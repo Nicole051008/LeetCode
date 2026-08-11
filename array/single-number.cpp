9ba192b2fceb697e445ff1dd61a852f1c8b8538e
Cola <hdmortalnil@gmail.com>
2026-08-11 23:17:03 +1000

    LC-single-number [Runtime: N/A; Memory: N/A]

--- array/single-number.cpp
+++ array/single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        for (int i = 0; i < nums.length; i ++){
+        for (int i = 0; i < nums.size(); i ++){
             result ^= nums[i];
         }
         return result;
