b16e510b9acc44100b6d42c45bb85fa3751a25e9
Cola <hdmortalnil@gmail.com>
2026-08-11 23:22:27 +1000

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

---
+++ math/palindrome-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 :
+            return false
+        
+        for front_p in len(str(x))/2:
+            back_p = len - 1 - front_p
+            if x[front_p] != x[back_p]:
+                return false
+        
+        return true
