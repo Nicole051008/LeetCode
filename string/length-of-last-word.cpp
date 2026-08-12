c0b03cdb857548af7ad9d4ae0c6f401948a610bb
Cola <hdmortalnil@gmail.com>
2026-08-12 22:04:16 +1000

    LC-length-of-last-word [Runtime: N/A; Memory: N/A]

---
+++ string/length-of-last-word.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = s.size() - 1;
+        int p = length;
+        while (s[p] != " " || p > 0){
+            p --;
+        }
+        return length - p;
+    }
+};
