d2689dfb689093f4c01bb260b7a9097189d4df4e
Cola <hdmortalnil@gmail.com>
2026-08-13 23:07:29 +1000

    LC-combine-two-tables [Runtime: N/A; Memory: N/A]

--- database/combine-two-tables.py
+++ database/combine-two-tables.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
-    result = person.merge(address, on ='personId',from = 'left')
+    result = person.merge(address, on ='personId', how = 'left')
     return result
