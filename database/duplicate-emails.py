ed1b2cdb74fa503cb515800f45b8d7d8c7c3a23a
Cola <hdmortalnil@gmail.com>
2026-08-14 21:14:58 +1000

    LC-duplicate-emails [Runtime: N/A; Memory: N/A]

--- database/duplicate-emails.py
+++ database/duplicate-emails.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
     result = person.groupby("email").size().reset_index(name = "count")
-    return result[result["count"] > 1]
+    result = result[result["count"] > 1]
+    return result[["email"]].rename(columns={"email": "Email"})
