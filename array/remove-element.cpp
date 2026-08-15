56000bca8416445ecf5dd3e49bd0716f68ce61c6
Cola <hdmortalnil@gmail.com>
2026-08-15 21:02:50 +1000

    LC-remove-element [Runtime: 0 ms; Memory: 11.8 MB]

--- array/remove-element.cpp
+++ array/remove-element.cpp
@@ -5,6 +5,7 @@ public:
         for (int i = 0; i < nums.size(); i++){
             if (nums[i] != val){
                 nums[count] = nums[i];
+                count ++;
             }
         }
         return count;
