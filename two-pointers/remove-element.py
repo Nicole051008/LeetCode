08fd7cba4818010ec60865216927ca02e85efd65
Cola <hdmortalnil@gmail.com>
2026-08-15 21:06:38 +1000

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- two-pointers/remove-element.py
+++ two-pointers/remove-element.py
@@ -6,9 +6,9 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range(nums):
+        for i in range(len(nums)):
             if nums[i] != val:
                 nums[count] = nums[i]
                 count += 1
-                
+
         return count
