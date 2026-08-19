b1e3d3e5a0860b8e3f8ec1db0de9a939f622a1d5
Cola <hdmortalnil@gmail.com>
2026-08-19 22:16:18 +1000

    LC-remove-duplicates-from-sorted-array [Runtime: N/A; Memory: N/A]

--- array/remove-duplicates-from-sorted-array.py
+++ array/remove-duplicates-from-sorted-array.py
@@ -5,14 +5,16 @@ class Solution(object):
         :rtype: int
         """
 
+        # init
         pointer = 0
         temp = None
 
+        # move unique elements to the front
         for i in range(len(nums)):
             if nums[i] != temp:
                 temp = nums[i]
                 nums[pointer] = nums[i]
                 pointer += 1
 
-        
+        # return the number of unique elements
         return pointer
