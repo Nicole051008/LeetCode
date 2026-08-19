1f302a4f147bd4bd5c6716eb6da87b98f9405d2b
Cola <hdmortalnil@gmail.com>
2026-08-19 22:10:08 +1000

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.java
+++ hash-table/two-sum.java
@@ -1,10 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
         int len = nums.length;
+
+        // check all the possible combination
         for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[]{i,j};
+                    return new int[]{i,j};  //return if found
                 }
             }
         }
