fdda9eca849054038227ac3ef58a1f467ed8c0e1
Cola <hdmortalnil@gmail.com>
2026-08-21 19:34:16 +1000

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- array/remove-element.cpp
+++ array/remove-element.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+
+        // init
         int count = 0;
-        for (int i = 0; i < nums.size(); i++){
-            if (nums[i] != val){
+
+        // move valid elements to the front
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
                 nums[count] = nums[i];
-                count ++;
+                count++;
             }
         }
+
+        // return the number of valid elements
         return count;
     }
 };
