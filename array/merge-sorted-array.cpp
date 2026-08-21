71a1931c4202ebf98185129897f8eb7f577f601b
Cola <hdmortalnil@gmail.com>
2026-08-21 19:37:10 +1000

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- array/merge-sorted-array.cpp
+++ array/merge-sorted-array.cpp
@@ -12,27 +12,31 @@ public:
         int nums1_pos = m - 1;
         int nums2_pos = n - 1;
 
-        for (int i = cur_pos; i >= 0; i --) {
+        // merge from the end of nums1
+        for (int i = cur_pos; i >= 0; i--) {
 
+            // copy nums2 if nums1 is exhausted
             if (nums1_pos < 0) {
                 nums1[i] = nums2[nums2_pos];
                 nums2_pos--;
             }
 
+            // place the larger element from nums1
             else if (nums1[nums1_pos] >= nums2[nums2_pos]) {
                 nums1[i] = nums1[nums1_pos];
-                nums1_pos --;
+                nums1_pos--;
             }
 
+            // place the larger element from nums2
             else {
-                 nums1[i] = nums2[nums2_pos];
-                 nums2_pos --;
-                 if (nums2_pos < 0) {
+                nums1[i] = nums2[nums2_pos];
+                nums2_pos--;
+
+                // stop if nums2 is exhausted
+                if (nums2_pos < 0) {
                     break;
-                 }
+                }
             }
-
-
         }
     }
 };
