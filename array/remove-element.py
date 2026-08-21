9ca1a884f021821b8e9959453d6de41cefeccd49
Cola <hdmortalnil@gmail.com>
2026-08-21 19:33:37 +1000

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- array/remove-element.py
+++ array/remove-element.py
@@ -5,10 +5,15 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+
+        # init
         count = 0
+
+        # move valid elements to the front
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[count] = nums[i]
                 count += 1
 
+        # return the number of valid elements
         return count
