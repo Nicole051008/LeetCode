ac13641cbefc2d3abef92e23f0ebca8a693e746a
Cola <hdmortalnil@gmail.com>
2026-08-21 19:33:58 +1000

    LC-remove-element [Runtime: N/A; Memory: N/A]

--- two-pointers/remove-element.java
+++ two-pointers/remove-element.java
@@ -1,12 +1,18 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+
+        // init
         int count = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] != val){
+
+        // move valid elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
                 nums[count] = nums[i];
-                count ++;
+                count++;
             }
         }
+
+        // return the number of valid elements
         return count;
     }
 }
