94f8c43c0fadbfec6da188bb5f90260605a8402f
Cola <hdmortalnil@gmail.com>
2026-08-22 20:41:19 +1000

    LC-single-number [Runtime: N/A; Memory: N/A]

--- array/single-number.cpp
+++ array/single-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+
+        // init
         int result = 0;
-        for (int i = 0; i < nums.size(); i ++){
+
+        // use XOR to cancel out duplicate numbers
+        for (int i = 0; i < nums.size(); i++) {
             result ^= nums[i];
         }
+
+        // return the single number
         return result;
     }
 };
