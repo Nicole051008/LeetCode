08a94836d68b6160fca9aec7a6c476ef8594dedf
Cola <hdmortalnil@gmail.com>
2026-08-22 20:41:39 +1000

    LC-single-number [Runtime: N/A; Memory: N/A]

--- bit-manipulation/single-number.py
+++ bit-manipulation/single-number.py
@@ -4,8 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        # init
         result = 0
+
+        # use XOR to cancel out duplicate numbers
         for num in nums:
             result ^= num
-        
+
+        # return the single number
         return result
