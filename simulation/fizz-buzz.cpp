db25f2e63c5aad2bc1839d44afd65367946d790a
Cola <hdmortalnil@gmail.com>
2026-09-01 20:46:34 +1000

    LC-fizz-buzz [Runtime: N/A; Memory: N/A]

---
+++ simulation/fizz-buzz.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+        // init
+        vector<string> result;
+
+        // append
+        for (int i = 0; i < n; i ++){
+
+            // if i divisible by 3 and 5
+            if (i % 3 == 0 || i % 5 == 0){
+                result.push_back("FizzBuzz")
+            }
+
+            // if i divisible by 3
+            else if (i % 3 == 0){
+                result.push_back("Fizz")
+            }
+
+            // if i divisible by 5
+            if (i % 5 == 0){
+                result.push_back("Buzz")
+            }
+
+            // satisfy none of the above conditions
+            else{
+                n = to_string(x)  // convert int to str
+                result.push_back("n")
+            }
+        }
+
+        return result
+    }
+};
